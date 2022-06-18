fa744d50591807b8cfccf580401546570ebc4734
mohammadyazarloo2 <34187298+mohammadyazarloo2@users.noreply.github.com>
2022-06-19 01:42:27 +0430

    home
    change home

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
 import Modal from './components/Modal';
-import Login from './components/Login/Login';
-import Insert from './components/Login/Insert';
-import View from './components/Login/View';
-import Edit from './components/Login/Edit';
 
 function App() {
   return (
@@ -23,7 +19,6 @@ function App() {
         <Route exact path="/" component={ProductList} />
         <Route path="/details" component={Details} />
         <Route path="/cart" component={Cart} />
-        <Route path="/insert" component={Insert} />
         <Route path="/edit/:id" component={Edit} />
         <Route path="/view" component={View} />
         <Route component={Default} />
